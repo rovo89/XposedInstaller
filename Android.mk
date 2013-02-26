18d62d959d071343c65ce696e86fad741f83b1f3
kecinzer <kecinzer@gmail.com>
2013-02-26 07:45:51 +0100

    Android.mk editing
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,8 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
-
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := XposedInstaller
