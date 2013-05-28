83182f46c910c95b8d418630c67c81ded64c6269
rovo89 <android@robv.de>
2013-05-28 19:41:47 +0200

    Downloader: correct indention in the RepoParser class

--- src/de/robv/android/xposed/installer/repo/RepoParser.java
+++ src/de/robv/android/xposed/installer/repo/RepoParser.java
@@ -13,103 +13,103 @@ public class RepoParser {
 	public final static String TAG = "XposedRepoParser";
 	protected final static String NS = null;
 	protected final XmlPullParser parser;
-	
+
 	public RepoParser(InputStream is) throws XmlPullParserException, IOException {
-        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-        parser = factory.newPullParser();
-        parser.setInput(is, null);
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		parser = factory.newPullParser();
+		parser.setInput(is, null);
 	}
-	
+
 	public Repository parse() throws XmlPullParserException, IOException {
-        parser.nextTag();
-        return readRepo();
+		parser.nextTag();
+		return readRepo();
 	}
-	
+
 	protected Repository readRepo() throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, NS, "repository");
-        Repository repository = new Repository();
+		parser.require(XmlPullParser.START_TAG, NS, "repository");
+		Repository repository = new Repository();
 
-        while (parser.nextTag() == XmlPullParser.START_TAG) {
-        	String tagName = parser.getName();
-        	if (tagName.equals("name")) {
-        		repository.name = parser.nextText(); 
-        	} else if (tagName.equals("module")) {
-        		Module module = readModule(repository);
-        		if (module != null)
-        			repository.modules.put(module.packageName, module);
-        	} else {
-        		skip();
-        	}
-        }
-        
-        return repository;
+		while (parser.nextTag() == XmlPullParser.START_TAG) {
+			String tagName = parser.getName();
+			if (tagName.equals("name")) {
+				repository.name = parser.nextText(); 
+			} else if (tagName.equals("module")) {
+				Module module = readModule(repository);
+				if (module != null)
+					repository.modules.put(module.packageName, module);
+			} else {
+				skip();
+			}
+		}
+
+		return repository;
 	}
-	
+
 	protected Module readModule(Repository repository) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, NS, "module");
-        Module module = new Module(repository);
-        module.packageName = parser.getAttributeValue(NS, "package");
-        if (module.packageName == null) {
-        	logError("no package name defined");
-        	return null;
-        }
+		parser.require(XmlPullParser.START_TAG, NS, "module");
+		Module module = new Module(repository);
+		module.packageName = parser.getAttributeValue(NS, "package");
+		if (module.packageName == null) {
+			logError("no package name defined");
+			return null;
+		}
 
-        while (parser.nextTag() == XmlPullParser.START_TAG) {
-        	String tagName = parser.getName();
-        	if (tagName.equals("name")) {
-        		module.name = parser.nextText();
-        	} else if (tagName.equals("author")) {
-        		module.author = parser.nextText();
-        	} else if (tagName.equals("contact")) {
-        		module.contact = parser.nextText();
-        	} else if (tagName.equals("description")) {
-        		module.description = parser.nextText();
-        	} else if (tagName.equals("screenshot")) {
-        		module.screenshots.add(parser.nextText());
-        	} else if (tagName.equals("version")) {
-        		ModuleVersion version = readModuleVersion(module);
-        		if (version != null)
-        			module.versions.add(version);
-        	} else {
-        		skip();
-        	}
-        }
-        
-        if (module.name == null) {
-        	logError("packages need at least a name");
-        	return null;
-        }
-        
-        return module;
+		while (parser.nextTag() == XmlPullParser.START_TAG) {
+			String tagName = parser.getName();
+			if (tagName.equals("name")) {
+				module.name = parser.nextText();
+			} else if (tagName.equals("author")) {
+				module.author = parser.nextText();
+			} else if (tagName.equals("contact")) {
+				module.contact = parser.nextText();
+			} else if (tagName.equals("description")) {
+				module.description = parser.nextText();
+			} else if (tagName.equals("screenshot")) {
+				module.screenshots.add(parser.nextText());
+			} else if (tagName.equals("version")) {
+				ModuleVersion version = readModuleVersion(module);
+				if (version != null)
+					module.versions.add(version);
+			} else {
+				skip();
+			}
+		}
+
+		if (module.name == null) {
+			logError("packages need at least a name");
+			return null;
+		}
+
+		return module;
 	}
-	
-	protected ModuleVersion readModuleVersion(Module module) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, NS, "version");
-        ModuleVersion version= new ModuleVersion(module);
 
-        while (parser.nextTag() == XmlPullParser.START_TAG) {
-        	String tagName = parser.getName();
-        	if (tagName.equals("name")) {
-        		version.name = parser.nextText();
-        	} else if (tagName.equals("code")) {
-        		try {
-        			version.code = Integer.parseInt(parser.nextText());
-        		} catch (NumberFormatException nfe) {
-        			logError(nfe.getMessage());
-        			return null;
-        		}
-        	} else if (tagName.equals("download")) {
-        		version.downloadLink = parser.nextText();
-        	} else if (tagName.equals("md5sum")) {
-        		version.md5sum = parser.nextText();
-        	} else if (tagName.equals("changelog")) {
-        		version.changelog.add(parser.nextText());
-        	} else {
-        		skip();
-        	}
-        }
-        
-        return version;
+	protected ModuleVersion readModuleVersion(Module module) throws XmlPullParserException, IOException {
+		parser.require(XmlPullParser.START_TAG, NS, "version");
+		ModuleVersion version= new ModuleVersion(module);
+
+		while (parser.nextTag() == XmlPullParser.START_TAG) {
+			String tagName = parser.getName();
+			if (tagName.equals("name")) {
+				version.name = parser.nextText();
+			} else if (tagName.equals("code")) {
+				try {
+					version.code = Integer.parseInt(parser.nextText());
+				} catch (NumberFormatException nfe) {
+					logError(nfe.getMessage());
+					return null;
+				}
+			} else if (tagName.equals("download")) {
+				version.downloadLink = parser.nextText();
+			} else if (tagName.equals("md5sum")) {
+				version.md5sum = parser.nextText();
+			} else if (tagName.equals("changelog")) {
+				version.changelog.add(parser.nextText());
+			} else {
+				skip();
+			}
+		}
+
+		return version;
 	}
 
 	protected void skip() throws XmlPullParserException, IOException {
@@ -125,7 +125,7 @@ public class RepoParser {
 			}
 		}
 	}
-	
+
 	protected void logError(String error) {
 		Log.e(TAG, parser.getPositionDescription() + ": " + error);
 	}
