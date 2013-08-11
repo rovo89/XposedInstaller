a9554e2a4e6900b4a905c15e53cfac8533b619b4
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2013-08-11 13:18:17 -0700

    display stderr as well for executed scripts
    This displays also errors caused by wrong calls and su denials
    ("Permission denied") which were not visible before.

--- src/de/robv/android/xposed/installer/InstallerFragment.java
+++ src/de/robv/android/xposed/installer/InstallerFragment.java
@@ -408,9 +408,14 @@ public class InstallerFragment extends Fragment {
 							+ scriptFile.getAbsolutePath() + " " + android.os.Process.myUid() + " 2>&1"
 					});
 			BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 			StringBuilder sb = new StringBuilder();
 			String line;
 			while ((line = stdout.readLine()) != null) {
+				sb.append(line);
+				sb.append('\n');
+			}
+			while ((line = stderr.readLine()) != null) {
 				sb.append(line);
 				sb.append('\n');
 			}
