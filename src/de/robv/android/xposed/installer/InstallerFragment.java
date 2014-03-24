9051981f5482e7a565621dac1a001ef9592385a2
GermainZ <germanosz@gmail.com>
2014-03-24 20:20:53 +0200

    Add category headers and make install mode a button

--- src/de/robv/android/xposed/installer/InstallerFragment.java
+++ src/de/robv/android/xposed/installer/InstallerFragment.java
@@ -19,14 +19,10 @@ import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
-import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,8 +47,8 @@ public class InstallerFragment extends Fragment {
 	private ProgressDialog dlgProgress;
 	private TextView txtAppProcessInstalledVersion, txtAppProcessLatestVersion;
 	private TextView txtJarInstalledVersion, txtJarLatestVersion;
-	private TextView txtInstallError, txtInstallMode;
-	private Button btnInstall, btnUninstall, btnSoftReboot, btnReboot;
+	private TextView txtInstallError;
+	private Button btnInstallMode, btnInstall, btnUninstall, btnSoftReboot, btnReboot;
 
 	private static final int INSTALL_MODE_NORMAL = 0;
 	private static final int INSTALL_MODE_RECOVERY_AUTO = 1;
@@ -79,7 +75,7 @@ public class InstallerFragment extends Fragment {
 		txtJarInstalledVersion = (TextView) v.findViewById(R.id.jar_installed_version);
 		txtJarLatestVersion = (TextView) v.findViewById(R.id.jar_latest_version);
 
-		txtInstallMode = (TextView) v.findViewById(R.id.framework_install_mode);
+		btnInstallMode = (Button) v.findViewById(R.id.framework_install_mode);
 		txtInstallError = (TextView) v.findViewById(R.id.framework_install_errors);
 
 		btnInstall = (Button) v.findViewById(R.id.btnInstall);
@@ -87,7 +83,7 @@ public class InstallerFragment extends Fragment {
 		btnSoftReboot = (Button) v.findViewById(R.id.btnSoftReboot);
 		btnReboot = (Button) v.findViewById(R.id.btnReboot);
 
-		txtInstallMode.setOnClickListener(new View.OnClickListener() {
+		btnInstallMode.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(getActivity(), XposedInstallerActivity.class);
@@ -204,12 +200,7 @@ public class InstallerFragment extends Fragment {
 	@Override
 	public void onResume() {
 		super.onResume();
-
-		SpannableStringBuilder installModeText = new SpannableStringBuilder(getString(R.string.settings_install_mode));
-		installModeText.append(":\n");
-		installModeText.setSpan(new StyleSpan(Typeface.BOLD), 0, installModeText.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-		installModeText.append(getInstallModeText());
-		txtInstallMode.setText(installModeText);
+		btnInstallMode.setText(getInstallModeText());
 	}
 
 	@Override
