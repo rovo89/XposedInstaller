6becd9e0156abe6b2f9fde436d039444090cf4a7
unknown <patton537@gmail.com>
2015-08-09 14:08:28 +0100

    welcome activity: fix issues, add snackbars to show updates and general cleanup

--- app/src/main/java/de/robv/android/xposed/installer/WelcomeActivity.java
+++ app/src/main/java/de/robv/android/xposed/installer/WelcomeActivity.java
@@ -1,14 +1,12 @@
 package de.robv.android.xposed.installer;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,17 +14,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+
 import de.robv.android.xposed.installer.util.ModuleUtil;
 import de.robv.android.xposed.installer.util.ModuleUtil.InstalledModule;
 import de.robv.android.xposed.installer.util.ModuleUtil.ModuleListener;
-import de.robv.android.xposed.installer.util.NavUtil;
 import de.robv.android.xposed.installer.util.RepoLoader;
 import de.robv.android.xposed.installer.util.RepoLoader.RepoListener;
 import de.robv.android.xposed.installer.util.ThemeUtil;
@@ -35,12 +26,10 @@ public class WelcomeActivity extends XposedBaseActivity implements
 		NavigationView.OnNavigationItemSelectedListener, ModuleListener, RepoListener {
 
 	private RepoLoader mRepoLoader;
-	private WelcomeAdapter mAdapter;
 
 	private static final String SELECTED_ITEM_ID = "SELECTED_ITEM_ID";
 
 	private Toolbar mToolbar;
-	//private final Handler mDrawerActionHandler = new Handler();
 	private DrawerLayout mDrawerLayout;
 	private NavigationView mNavigationView;
 	private ActionBarDrawerToggle mDrawerToggle;
@@ -56,11 +45,9 @@ public class WelcomeActivity extends XposedBaseActivity implements
 		mToolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(mToolbar);
 
-		// listen for navigation events
-		NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+		mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
 		mNavigationView.setNavigationItemSelectedListener(this);
 
-		// set up the hamburger icon to open and close the drawer
 		mDrawerToggle = new ActionBarDrawerToggle(this,
 				mDrawerLayout,
 				mToolbar,
@@ -79,35 +66,11 @@ public class WelcomeActivity extends XposedBaseActivity implements
 			navigate(mSelectedId);
 		}
 
-		//requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		mRepoLoader = RepoLoader.getInstance();
-/*
-		mAdapter = new WelcomeAdapter(this);
-		// TODO add proper description texts and load them from resources, add icons, make it more fancy, ...
-		mAdapter.add(new WelcomeItem(R.string.tabInstall, R.string.tabInstallDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabModules, R.string.tabModulesDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabDownload, R.string.tabDownloadDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabSettings, R.string.tabSettingsDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabSupport, R.string.tabSupportDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabLogs, R.string.tabLogsDescription));
-		mAdapter.add(new WelcomeItem(R.string.tabAbout, R.string.tabAboutDescription));
-/*
-		ListView lv = (ListView) findViewById(R.id.welcome_list);
-		lv.setAdapter(mAdapter);
-		lv.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Intent intent = new Intent(WelcomeActivity.this, XposedInstallerActivity.class);
-				intent.putExtra(XposedInstallerActivity.EXTRA_SECTION, position);
-				intent.putExtra(NavUtil.FINISH_ON_UP_NAVIGATION, true);
-				startActivity(intent);
-				NavUtil.setTransitionSlideEnter(WelcomeActivity.this);
-			}
-		});
-*/
-
 		ModuleUtil.getInstance().addListener(this);
 		mRepoLoader.addListener(this, false);
+
+		notifyDataSetChanged();
 	}
 
 	private void navigate(final int itemId) {
@@ -150,11 +113,8 @@ public class WelcomeActivity extends XposedBaseActivity implements
 	public boolean onNavigationItemSelected(MenuItem menuItem) {
 		menuItem.setChecked(true);
 		mSelectedId = menuItem.getItemId();
-
 		mDrawerLayout.closeDrawer(GravityCompat.START);
-
 		navigate(mSelectedId);
-
 		return true;
 	}
 
@@ -173,19 +133,32 @@ public class WelcomeActivity extends XposedBaseActivity implements
 		}
 	}
 
-	//ToDo fix this, we don't use mAdapter any more
-	//remove during cleanup after testing
 	private void notifyDataSetChanged() {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				if (mAdapter != null) {
-					synchronized (mAdapter) {
-						mAdapter.notifyDataSetChanged();
-					}
-				}
+		View parentLayout = findViewById(R.id.content_frame);
+		String frameworkUpdateVersion = mRepoLoader.getFrameworkUpdateVersion();
+		boolean moduleUpdateAvailable = mRepoLoader.hasModuleUpdates();
+
+		Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.content_frame);
+		if (currentFragment instanceof DownloadDetailsFragment) {
+			if (frameworkUpdateVersion != null) {
+				Snackbar.make(parentLayout,
+						R.string.welcome_framework_update_available + " " +
+								String.valueOf(frameworkUpdateVersion),
+						Snackbar.LENGTH_LONG).show();
 			}
-		});
+		}
+
+		if (moduleUpdateAvailable) {
+			Snackbar.make(parentLayout, R.string.modules_updates_available, Snackbar.LENGTH_LONG)
+					.setAction("VIEW", new View.OnClickListener() {
+						@Override
+						public void onClick(View view) {
+							mSelectedId = mNavigationView.getMenu().getItem(2).getItemId();
+							mNavigationView.getMenu().findItem(mSelectedId).setChecked(true);
+							navigate(mSelectedId);
+						}
+					}).show();
+		}
 	}
 
 	@Override
@@ -208,57 +181,5 @@ public class WelcomeActivity extends XposedBaseActivity implements
 		super.onDestroy();
 		ModuleUtil.getInstance().removeListener(this);
 		mRepoLoader.removeListener(this);
-	}
-
-	class WelcomeAdapter extends ArrayAdapter<WelcomeItem> {
-		public WelcomeAdapter(Context context) {
-			super(context, R.layout.list_item_welcome, android.R.id.text1);
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = super.getView(position, convertView, parent);
-			WelcomeItem item = getItem(position);
-
-			TextView description = (TextView) view.findViewById(android.R.id.text2);
-			description.setText(item.description);
-
-			boolean xposedActive = true;
-			String frameworkUpdateVersion = null;
-			boolean moduleUpdateAvailable = false;
-			//ToDo fix this
-			/*if (position == XposedInstallerActivity.TAB_INSTALL) {
-				xposedActive = XposedApp.getActiveXposedVersion() > 0;
-			} else if (position == XposedInstallerActivity.TAB_DOWNLOAD) {
-				frameworkUpdateVersion = mRepoLoader.getFrameworkUpdateVersion();
-				moduleUpdateAvailable = mRepoLoader.hasModuleUpdates();
-
-				if (frameworkUpdateVersion != null) {
-					((TextView) view.findViewById(R.id.txtFrameworkUpdateAvailable)).setText(
-						getResources().getString(R.string.welcome_framework_update_available, (String)frameworkUpdateVersion));
-				}
-			}*/
-
-			view.findViewById(R.id.txtXposedNotActive).setVisibility(!xposedActive ? View.VISIBLE : View.GONE);
-			view.findViewById(R.id.txtFrameworkUpdateAvailable).setVisibility(frameworkUpdateVersion != null ? View.VISIBLE : View.GONE);
-			view.findViewById(R.id.txtUpdateAvailable).setVisibility(moduleUpdateAvailable ? View.VISIBLE : View.GONE);
-
-			return view;
-		}
-	}
-
-	class WelcomeItem {
-		public final String title;
-		public final String description;
-
-		protected WelcomeItem(int titleResId, int descriptionResId) {
-			this.title = getString(titleResId);
-			this.description = getString(descriptionResId);
-		}
-
-		@Override
-		public String toString() {
-			return title;
-		}
 	}
 }
