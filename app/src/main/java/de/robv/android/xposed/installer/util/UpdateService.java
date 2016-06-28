9a0b1dc451930dffa8ebc8d005e92ad06f305aa9
pyler <pyler@azet.sk>
2016-06-28 19:31:52 +0200

    Option to disable background sync (#83)

--- app/src/main/java/de/robv/android/xposed/installer/util/UpdateService.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/UpdateService.java
@@ -55,7 +55,7 @@ public class UpdateService extends Service {
 
         SharedPreferences prefs = getSharedPreferences(getPackageName() + "_preferences", MODE_PRIVATE);
         int interval = Integer.parseInt(prefs.getString("update_service_interval", "3"));
-
+        if (interval == -1) return;
         mTimer = new Timer();
         mTimer.schedule(mTask, 2000, interval * 60 * 60 * 1000);
     }
