048b5967244bf5b2e885e1ab48e6efdfb2f01ca7
dvdandroid <davide.intelligent@gmail.com>
2016-07-26 09:38:58 +0200

    Removed root shell, added systemless check, print only error message

--- app/src/main/java/de/robv/android/xposed/installer/installation/InstallationActivity.java
+++ app/src/main/java/de/robv/android/xposed/installer/installation/InstallationActivity.java
@@ -4,14 +4,12 @@ import android.animation.Animator;
 import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewAnimationUtils;
@@ -20,21 +18,15 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-
-import java.util.LinkedList;
-import java.util.List;
+import java.io.File;
 
 import de.robv.android.xposed.installer.R;
 import de.robv.android.xposed.installer.XposedBaseActivity;
-import de.robv.android.xposed.installer.util.AssetUtil;
-import de.robv.android.xposed.installer.util.RootUtil;
 import de.robv.android.xposed.installer.util.ThemeUtil;
 
 public class InstallationActivity extends XposedBaseActivity {
 
     private static String mPath;
-    private static RootUtil mRootUtil = new RootUtil();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,73 +71,13 @@ public class InstallationActivity extends XposedBaseActivity {
         private ImageView mConsoleResult;
         private CardView mResultContainer;
 
-        private boolean startShell() {
-            if (mRootUtil.startShell())
-                return true;
-
-            showAlert(getString(R.string.root_failed));
-            return false;
-        }
-
-        private void showAlert(final String result) {
-            if (Looper.myLooper() != Looper.getMainLooper()) {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        showAlert(result);
-                    }
-                });
-                return;
-            }
-
-            MaterialDialog dialog = new MaterialDialog.Builder(getActivity()).content(result).positiveText(android.R.string.ok).build();
-            dialog.show();
-
-            TextView txtMessage = (TextView) dialog
-                    .findViewById(android.R.id.message);
-            try {
-                txtMessage.setTextSize(14);
-            } catch (NullPointerException ignored) {
-            }
-        }
-
-        private void areYouSure(int contentTextId, MaterialDialog.ButtonCallback yesHandler) {
-            new MaterialDialog.Builder(getActivity()).title(R.string.areyousure)
-                    .content(contentTextId)
-                    .iconAttr(android.R.attr.alertDialogIcon)
-                    .positiveText(android.R.string.yes)
-                    .negativeText(android.R.string.no).callback(yesHandler).show();
-        }
-
-        private void reboot(String mode) {
-            if (!startShell())
-                return;
-
-            List<String> messages = new LinkedList<>();
-
-            String command = "reboot";
-            if (mode != null) {
-                command += " " + mode;
-                if (mode.equals("recovery"))
-                    // create a flag used by some kernels to boot into recovery
-                    mRootUtil.executeWithBusybox("touch /cache/recovery/boot", messages);
-            }
-
-            if (mRootUtil.executeWithBusybox(command, messages) != 0) {
-                messages.add("");
-                messages.add(getString(R.string.reboot_failed));
-                showAlert(TextUtils.join("\n", messages).trim());
-            }
-            AssetUtil.removeBusybox();
-        }
-
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setRetainInstance(true);
 
             if (savedInstanceState == null) {
-                InstallDirect.install(mPath, this, mPath.contains("systemless"));
+                InstallDirect.install(mPath, this, isOkSystemless());
             }
         }
 
@@ -164,6 +96,8 @@ public class InstallationActivity extends XposedBaseActivity {
         @Override
         public void onStarted() {
             appendText(getString(R.string.installation_started), TYPE_NONE);
+            appendText(getString(R.string.systemless_mode, isOkSystemless()), TYPE_NONE);
+
             mProgress.setIndeterminate(true);
         }
 
@@ -186,21 +120,11 @@ public class InstallationActivity extends XposedBaseActivity {
             //noinspection deprecation
             mResultContainer.setCardBackgroundColor(getResources().getColor(R.color.darker_green));
             animateResult();
-
-
-            areYouSure(R.string.reboot, new MaterialDialog.ButtonCallback() {
-                @Override
-                public void onPositive(MaterialDialog dialog) {
-                    super.onPositive(dialog);
-                    reboot(null);
-                }
-            });
         }
 
         @Override
-        @SuppressLint("DefaultLocale")
         public void onError(int exitCode, String error) {
-            appendText(String.format("%d %s", exitCode, error), TYPE_ERROR);
+            appendText(error, TYPE_ERROR);
 
             mProgress.setIndeterminate(false);
             mConsoleResult.setImageResource(R.drawable.ic_error);
@@ -245,6 +169,19 @@ public class InstallationActivity extends XposedBaseActivity {
             } else {
                 mLogText.setText(Html.fromHtml(text), TextView.BufferType.EDITABLE);
             }
+        }
+
+        private boolean isOkSystemless() {
+            boolean suPartition = new File("/su").exists() && new File("/data/su.img").exists();
+            boolean m = Build.VERSION.SDK_INT >= 23; //redundant
+
+            /*
+            TODO: Add toggle for user to force system installation
+
+            This can be done with a "nosystemless" file in app's folder and with a check in update-binary
+             */
+
+            return m && suPartition;
         }
     }
 }
