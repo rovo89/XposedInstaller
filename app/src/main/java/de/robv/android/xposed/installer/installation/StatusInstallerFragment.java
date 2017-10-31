c66b154d324fb0a72599c8ad3bcf4d9e8015996c
rovo89 <android@robv.de>
2017-10-31 10:40:37 +0100

    Mention wrong base directory in the error message
    Chances are that the log isn't easily available in this situation, so
    better show it in the UI directly.

--- app/src/main/java/de/robv/android/xposed/installer/installation/StatusInstallerFragment.java
+++ app/src/main/java/de/robv/android/xposed/installer/installation/StatusInstallerFragment.java
@@ -252,12 +252,12 @@ public class StatusInstallerFragment extends Fragment {
         } else if (Build.VERSION.SDK_INT < 24 && !baseDir.equals(new File(appInfo.dataDir))) {
             Log.e(XposedApp.TAG, "Base directory: " + appInfo.dataDir);
             Log.e(XposedApp.TAG, "Expected: " + XposedApp.BASE_DIR);
-            issueName = getString(R.string.known_issue_wrong_base_directory);
+            issueName = getString(R.string.known_issue_wrong_base_directory, appInfo.dataDir);
             issueLink = "https://github.com/rovo89/XposedInstaller/issues/395";
         } else if (Build.VERSION.SDK_INT >= 24 && !baseDir.equals(new File(appInfo.deviceProtectedDataDir))) {
             Log.e(XposedApp.TAG, "Base directory: " + appInfo.deviceProtectedDataDir);
             Log.e(XposedApp.TAG, "Expected: " + XposedApp.BASE_DIR);
-            issueName = getString(R.string.known_issue_wrong_base_directory);
+            issueName = getString(R.string.known_issue_wrong_base_directory, appInfo.deviceProtectedDataDir);
             issueLink = "https://github.com/rovo89/XposedInstaller/issues/395";
         } else if (!baseDir.exists()) {
             issueName = getString(R.string.known_issue_missing_base_directory);
